8c72538d87c91f6cf120b3b3dd5ef4cd3ef05615
teuespaco <69022555+teuespaco@users.noreply.github.com>
2023-11-30 08:33:56 -0300

    Delete SISTEMAS DISTRIBUÍDOS directory

--- SISTEMAS DISTRIBUÍDOS/Algoritmo-Berkeley-Unix-main/launcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start cmd /k "mode con: cols=100 lines=100 && python main_server.py 5000"
-start cmd /k "mode con: cols=100 lines=11 && python main_client.py 5001"
-start cmd /k "mode con: cols=100 lines=11 && python main_client.py 5002"
-start cmd /k "mode con: cols=100 lines=10 && python main_client.py 5003"
-start cmd /k "mode con: cols=100 lines=11 && python main_client.py 5004"
--- SISTEMAS DISTRIBUÍDOS/Web_Service-REST-main/Web_Service-REST-main/web_service/rest/api/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from rest.api import serializers
-from rest.models import Data
-
-class DataViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.DataSerializer
-    queryset = Data.objects.all()
-    
